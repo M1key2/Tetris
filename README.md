8f75d47f2f4f7e5ddd3d0af4beca1af85975984f
Miguel Angel Sanchez Peñuñuri <166767772+M1key2@users.noreply.github.com>
2024-11-12 12:51:27 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# TetrisGames
---
+++ TetrisGames/Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+
+
+namespace TetrisGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡ Inicia el juego Tetris !");
+            using (TetrisGame game = new TetrisGame(800, 600, "Tetris 2D"))
+            {
+                game.Run();
+            }
+        }
+    }
+}
